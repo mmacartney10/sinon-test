92cc4fe808bd76e458580763af626df847a242b8
Tony McBeth <tony.mcbeth@gmail.com>
2017-04-26 12:08:18 +0100

    merged master

--- flight-api.js
+++ flight-api.js
@@ -5,7 +5,7 @@ var FlightApi = function(apiClient) {
       return apiClient.get({
         url: 'http://api.magairports.com/v1/flights/departures/${airportCode}',
         headers: {
-          'x-api-key': '8id4sXD3SoGZh634sukY1IlbcthFcHUCbRSLnoc0'
+          'x-api-key': ''
         }
       });
     }
